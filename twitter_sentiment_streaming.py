516760f60c898cff4ba31b1ead31e0e677315a85
Jared Connor <jaredconnor301@gmail.com>
2018-05-20 15:30:41 -0700

    querying twitter sentiment data via tweepy and vader sentiment analysis

---
+++ twitter_sentiment_streaming.py
@@ -0,0 +1,51 @@
+from tweepy import Stream
+from tweepy import OAuthHandler
+from tweepy.streaming import StreamListener
+import sqlite3 as sql
+import json
+from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
+from unidecode import unidecode
+
+analyzer = SentimentIntensityAnalyzer()
+
+conn = sql.connect('twitter_sentiment.db')
+cursor = conn.cursor()
+
+def create_table():
+    cursor.execute("CREATE TABLE IF NOT EXISTS sentiment(unix REAL, tweet TEXT, sentiment REAL)")
+    conn.commit()
+
+create_table()
+
+consumer_key = "6JvfDG04UCFlpYThWPBYD43U8"
+consumer_secret = "cYN3bPUthnoiaiZJjHWHK8VTLF4wk32wTg8TmKIpMwtIcIGZa7"
+access_token = "444822244-BnyUlvHCrRJ0gB7LLgZOuH96aSfMrL9f8Mi8mXRT"
+access_secret = "EWpuLcNJ3WcNCmBQ17b1CBwPtAviBQyb0jgI9KGdZvsiU"
+
+class listener(StreamListener):
+
+    def on_data(self,data):
+        try:
+            data = json.loads(data)
+            tweet = unidecode(data['text'])
+            time_ms = data['timestamp_ms']
+            vs = analyzer.polarity_scores(tweet)
+            sentiment = vs['compound']
+            print(time_ms, tweet, sentiment)
+            cursor.execute("INSERT INTO sentiment (unix, tweet, sentiment) VALUES (?, ?, ?)",
+                        (time_ms, tweet, sentiment))
+            conn.commit()
+
+        except KeyError as e:
+            print(str(e))
+        return(True)
+
+
+    def on_error(self, status):
+        print(status)
+
+auth = OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_secret)
+
+twitterStream = Stream(auth, listener())
+twitterStream.filter(track = ["a","e","i","o","u"])
